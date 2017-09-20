da771fdb90950d65924986256160e10a720a891f
李力 <lili@cangoonline.com>
2017-09-20 14:23:12 +0800

    添加首页底部

--- PalmCarTreasure/adpickcar/src/main/java/com/cango/adpickcar/main/MainFragment.java
+++ PalmCarTreasure/adpickcar/src/main/java/com/cango/adpickcar/main/MainFragment.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.cango.adpickcar.R;
@@ -37,7 +38,7 @@ public class MainFragment extends BaseFragment {
     @BindView(R.id.drawer_main)
     DrawerLayout mDrawerLayout;
     @BindView(R.id.rl_drawer)
-    RelativeLayout rlDrawer;
+    LinearLayout rlDrawer;
     @BindView(R.id.toolbar_main)
     Toolbar mToolbar;
     @BindView(R.id.recyclerview_main)
